042a83d1356c99f33581e5870fe2f0988a8614e4
Matias Carrillo Sanchez <Matiascarrillosanchez17@gmail.com>
2024-09-27 19:03:51 -0500

    Coneccion con base de enter establecida

--- apps/backend/models/User.js
+++ apps/backend/models/User.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     fullname: { type: String, required: true },
-    phone: { type: String, required: true }, 
-    skills: { type: [String], default: [] } 
-});
+    phone: { type: String, required: true },
+    skills: { type: [String], default: [] }
+}, { collection: "user_skills" });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
