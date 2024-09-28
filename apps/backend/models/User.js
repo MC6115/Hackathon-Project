172a3798d78ec75817ab20034643e89c16106820
Matias Carrillo Sanchez <Matiascarrillosanchez17@gmail.com>
2024-09-27 20:07:33 -0500

    changes to backend for vercel backend usage

--- apps/backend/models/User.js
+++ apps/backend/models/User.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
+const skillSchema = new mongoose.Schema({
+    name:{type:String},
+    level:{type:String}
+})
+
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     fullname: { type: String, required: true },
     phone: { type: String, required: true },
-    skills: { type: [String], default: [] }
+    skills: [skillSchema]
 }, { collection: "user_skills" });
 
 const User = mongoose.model("User", userSchema);
