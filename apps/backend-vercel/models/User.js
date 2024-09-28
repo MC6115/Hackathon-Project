ada9ae6c5027f5ee4bc878a051a77c479e573732
Matias Carrillo Sanchez <Matiascarrillosanchez17@gmail.com>
2024-09-27 19:56:39 -0500

    vercel backend

---
+++ apps/backend-vercel/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    fullname: { type: String, required: true },
+    phone: { type: String, required: true },
+    skills: { type: [String], default: [] }
+}, { collection: "user_skills" });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
